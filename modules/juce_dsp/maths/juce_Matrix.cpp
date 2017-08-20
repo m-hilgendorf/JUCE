53b2247753c7260609bb21a4dfb63ffa83d82c66
m-hilgendorf <mike@hilgendorf.audio>
2017-08-20 11:21:03 -0400

    Fixes to Matrix so it's not totally broken
    These errors popped up in Xcode 8.3.3. 
    
    Attempting to use any of the methods in dsp::Matrix that were defined in Matrix.cpp would not compile. Fixed by adding explicit template instantiation. Alternative fix is to add `#include "juce_Matrix.cpp"` at the bottom of juce_Matrix.h, but I don't think you guys really want people instantiating matrices of any type. 
    
    An additional compilation error at line 280 read  "Member function 'getRawDataPointer' not viable: 'this' argument has type 'const Array<float>', but function is not marked const." I noticed an additional overload of getRawDataPointer was marked const, tried to declare `p` to be `const ElementType*`, and still got compile errors.  Fixed by just pasting the body of the `const` definition of `getRawDataPointer()`. This might be an Xcode thing.

--- modules/juce_dsp/maths/juce_Matrix.cpp
+++ modules/juce_dsp/maths/juce_Matrix.cpp
@@ -277,7 +277,7 @@ String Matrix<ElementType>::toString() const
     StringArray entries;
     int sizeMax = 0;
 
-    auto* p = data.getRawDataPointer();
+    auto* p = data.begin();
 
     for (size_t i = 0; i < rows; ++i)
     {
@@ -306,3 +306,6 @@ String Matrix<ElementType>::toString() const
 
     return result.toString();
 }
+
+template struct dsp::Matrix <float>;
+template struct dsp::Matrix <double>;
